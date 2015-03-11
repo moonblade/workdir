533ab8754db180ee404d9da1058eb9c6882a6ee4
moonblade <moonblade168@gmail.com>
2015-03-11 17:40:50 +0530

    confirmation single page

---
+++ dhwani/Confirm.php
@@ -0,0 +1,58 @@
+<?PHP
+include "db_details.php";
+$id = $_POST['id'];
+function not_in($row)
+{
+	if ($row['inside']=="Null")
+		return 1;
+	else
+		return 0;
+}
+mysql_connect($server, $user_name, $password);
+$db_handle = mysql_connect($server, $user_name, $password);
+
+$db_found = mysql_select_db($database, $db_handle);
+if ($db_found) 
+{
+	$time=time();
+	$boo=new DateTime("@$time");
+	print "Current Time is ".$boo->format("Y-m-d H:i:s")."<br><br>";	
+	foreach ($id as $i) {
+
+
+		if (strlen($i)!=0)
+		{
+			$SQL = "SELECT * FROM $tablename where $id_attribute = '$i'";
+			$result = mysql_query($SQL) or die(mysql_error());
+			$num_rows = mysql_num_rows($result);
+			$row = mysql_fetch_assoc($result);
+			if ($num_rows > 0) {
+				$gettime="SELECT $inside_attribute,name from $tablename where id='$i'";
+				$timeresult=mysql_query($gettime) or die(mysql_error());
+				$timerow = mysql_fetch_assoc($timeresult);
+				$name=$timerow[$name_attribute];
+				{
+					?>
+					<!DOCTYPE html>
+					<html>
+					<body>
+						<center>
+							<form method="post" action="Entry.php" >
+								<?php echo "id : ".$i.", Name : ".$name;?>
+								<input type="hidden" name="id" value="<?php echo $i;?>">
+								<input class="button" type="submit" value="Confirm" />
+
+							</form>
+						</center>
+					</body>
+					</html>
+					</html>
+					<?PHP 
+				} 
+			// print $message."</br>";
+			} 
+		}
+	}
+}
+mysql_close($db_handle);
+?>
